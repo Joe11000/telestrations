71e38719a2c2542dd5b6d69620c7cc811ac025c1
Joe Noonan <joenoonan27@gmail.com>
2016-05-18 22:47:35 -0500

    only connect to stream if on the right page

--- app/channels/game_channel.rb
+++ app/channels/game_channel.rb
@@ -1,9 +1,7 @@
 # Be sure to restart your server when you modify this file. Action Cable runs in a loop that does not support auto reloading.
 class GameChannel < ApplicationCable::Channel
   def subscribed
-    stream_from "game_#{params[:game_id]}"
-
-
+    stream_from "game_#{params[:game_id]}" unless params[:game_id].blank?
   end
 
   def unsubscribed
