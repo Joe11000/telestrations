fee5a67eee09d4a46e01bbbc977c2efae8f6e4bd
Joe Noonan <joenoonan27@gmail.com>
2019-01-01 17:10:31 -0600

    fixing breaking tests

--- app/javascript/packs/postgame/SlideshowList.jsx
+++ app/javascript/packs/postgame/SlideshowList.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Slideshow from './Slideshow'
 import $ from 'jquery'
+import { ListGroup, ListGroupItem } from 'reactstrap';
 
 // import LoadingIcon from 'images/loading_icon.gif'
 
@@ -11,19 +12,17 @@ export default class SlideshowList extends React.Component {
     var that_props = this.props;
 
     return (
-      <div id='slideshow-list'>
-        <ul class="list-group list-group-flush">
-          {
-            this.props.arr_of_postgame_card_set && this.props.arr_of_postgame_card_set.map(function(deck, index){
-              return (
-                <li class="list-group-item" key={`list-group-item-${index}`}>
-                  <Slideshow deck={deck} current_user_info={that_props.current_user_info} key_preface={`slideshow-${index}`} />
-                </li>
-              )
-            })
-          }
-        </ul>
-      </div>
+      <ListGroup flush>
+        {
+          this.props.arr_of_postgame_card_set && this.props.arr_of_postgame_card_set.map(function(deck, index){
+            return (
+              <ListGroupItem  class="list-group-item" key={`list-group-item-${index}`}>
+                <Slideshow deck={deck} current_user_info={that_props.current_user_info} key_preface={`slideshow-${index}`} />
+              </ListGroupItem>
+            )
+          })
+        }
+      </ListGroup>
     )
   }
 }
--- spec/javascript/packs/Slideshow.spec.js
+++ spec/javascript/packs/Slideshow.spec.js
@@ -7,60 +7,61 @@ describe('Slideshow component', () => {
   it('renders', () => {
     const props = {
                     'deck': [
-                              'Current_Users__Games_User_Name',
-                              {
-                                'created_at': "2018-12-09T23:21:04.669Z",
-                                'deleted_at': null,
-                                'description_text': "excellent burnt orange tartan ham",
-                                'id': 55,
-                                'idea_catalyst_id': 19,
-                                'medium': "description",
-                                'out_of_game_card_upload': false,
-                                'parent_card_id': null,
-                                'placeholder': false,
-                                'starting_games_user_id': 19,
-                                'updated_at': "2018-12-09T23:21:04.985Z",
-                                'uploader_id': 1,
-                              }
-                            ],
-                            [
-                              "Devon Kreiger I",
-                              {
-                                'created_at': "2018-12-09T23:21:05.257Z",
-                                'deleted_at': null,
-                                'description_text': null,
-                                'id': 58,
-                                'idea_catalyst_id': null,
-                                'medium': "drawing",
-                                'out_of_game_card_upload': false,
-                                'parent_card_id': 55,
-                                'placeholder': false,
-                                'starting_games_user_id': 19,
-                                'updated_at': "2018-12-09T23:21:05.818Z",
-                                'uploader_id': 14,
-                              }
-                            ],
-                            [
-                              "Lincoln Cartwright IV",
-                              {
-                                'created_at': "2018-12-09T23:21:05.853Z",
-                                'deleted_at': null,
-                                'description_text': "old fashioned jade plaid ant",
-                                'id': 62,
-                                'idea_catalyst_id': null,
-                                'medium': "description",
-                                'out_of_game_card_upload': false,
-                                'parent_card_id': 58,
-                                'placeholder': false,
-                                'starting_games_user_id': 19,
-                                'updated_at': "2018-12-09T23:21:05.856Z",
-                                'uploader_id': 15,
-                              }
+                              [
+                                'Current_Users__Games_User_Name',
+                                {
+                                  'created_at': "2018-12-09T23:21:04.669Z",
+                                  'deleted_at': null,
+                                  'description_text': "excellent burnt orange tartan ham",
+                                  'id': 55,
+                                  'idea_catalyst_id': 19,
+                                  'medium': "description",
+                                  'out_of_game_card_upload': false,
+                                  'parent_card_id': null,
+                                  'placeholder': false,
+                                  'starting_games_user_id': 19,
+                                  'updated_at': "2018-12-09T23:21:04.985Z",
+                                  'uploader_id': 1,
+                                }
+                              ],
+                              [
+                                "Devon Kreiger I",
+                                {
+                                  'created_at': "2018-12-09T23:21:05.257Z",
+                                  'deleted_at': null,
+                                  'description_text': null,
+                                  'id': 58,
+                                  'idea_catalyst_id': null,
+                                  'medium': "drawing",
+                                  'out_of_game_card_upload': false,
+                                  'parent_card_id': 55,
+                                  'placeholder': false,
+                                  'starting_games_user_id': 19,
+                                  'updated_at': "2018-12-09T23:21:05.818Z",
+                                  'uploader_id': 14,
+                                }
+                              ],
+                              [
+                                "Lincoln Cartwright IV",
+                                {
+                                  'created_at': "2018-12-09T23:21:05.853Z",
+                                  'deleted_at': null,
+                                  'description_text': "old fashioned jade plaid ant",
+                                  'id': 62,
+                                  'idea_catalyst_id': null,
+                                  'medium': "description",
+                                  'out_of_game_card_upload': false,
+                                  'parent_card_id': 58,
+                                  'placeholder': false,
+                                  'starting_games_user_id': 19,
+                                  'updated_at': "2018-12-09T23:21:05.856Z",
+                                  'uploader_id': 15,
+                                }
+                              ]
                             ]
-                          ]
                   }
 
-    const postgame = shallow(<Slideshow {..props} />)
+    const postgame = shallow(<Slideshow {...props} />)
 
     expect( postgame.contains('.card') ).toBeDefined();
     // expect( postgame.contains('.card-header .nav-item') ).to.have.lengthOf(2);
