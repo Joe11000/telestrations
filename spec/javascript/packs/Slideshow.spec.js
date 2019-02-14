64c9b130152732333108e54ce048609db9d8cd1e
Joe Noonan <joenoonan27@gmail.com>
2019-02-14 10:58:15 -0600

    Add new Slideshow.spec.js test

--- spec/javascript/packs/Slideshow.spec.js
+++ spec/javascript/packs/Slideshow.spec.js
@@ -27,39 +27,55 @@ describe('Slideshow component', () => {
     });
 
 
-    it("first card has correct content", () => {
-      const props_1 = { deck: mock_games_index_request.arr_of_postgame_card_set[0], 
-        current_user_info: mock_games_index_request.current_user_info }
-      const slideshow_component_1 = mount(<Slideshow {...props_1} />);
-
-      const carousel_item = slideshow_component_1.find('.carousel .carousel-item').at(0);
-      const description = carousel_item.find('.card-body p').at(0).text(); 
-      const author = carousel_item.find('.card-body p').at(1).text();
+    describe('Cards have correct content', () => {
+      it("first card has correct content", () => {
+        const props_1 = { deck: mock_games_index_request.arr_of_postgame_card_set[0], 
+          current_user_info: mock_games_index_request.current_user_info }
+          
+          const slideshow_component_1 = mount(<Slideshow {...props_1} />);
+          const carousel_item = slideshow_component_1.find('.carousel .carousel-item').at(0);
+          const description = carousel_item.find('.card-body p').at(0).text(); 
+          const author = carousel_item.find('.card-body p').at(1).text();
+          
+          const expected_description = "excellent burnt orange tartan ham";
+          const expected_author = "By: Zackary Jaskolski";
+        
+        expect(description).toEqual(expected_description);
+        expect(author).toEqual(expected_author);
+      });
+  
+      it('second card has correct content', () => {
+        const props_2 = { deck: mock_games_index_request.arr_of_postgame_card_set[0], 
+          current_user_info: mock_games_index_request.current_user_info }
+        const slideshow_component_1 = mount(<Slideshow {...props_2} />);
+        
+        
+        const carousel_item = slideshow_component_1.find('.carousel .carousel-item').at(1);
+        const url = carousel_item.find('img').prop('src');
+        const author = carousel_item.find('p').at(1).text();
+        
+        const expected_url = "/rails/active_storage/blobs/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBOZz09IiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--0f3f1adfa5d3d579512daee958eacd7b6a98d277/provider_avatar.jpg?disposition=attachment"
+        const expected_author = "By: Devon Kreiger I";
+  
+        expect(url).toEqual(expected_url);
+        expect(author).toEqual(expected_author);
+      });
+  
+      it("third card has correct content", () => {
+        const props_3 = { deck: mock_games_index_request.arr_of_postgame_card_set[0], 
+          current_user_info: mock_games_index_request.current_user_info }
+        const slideshow_component_1 = mount(<Slideshow {...props_3} />);
+  
+        const carousel_item = slideshow_component_1.find('.carousel .carousel-item').at(2);
+        const description = carousel_item.find('.card-body p').at(0).text(); 
+        const author = carousel_item.find('.card-body p').at(1).text();
+        
+        const expected_description = "old fashioned jade plaid ant";
+        const expected_author = "By: Lincoln Cartwright IV";
       
-      const expected_description = "excellent burnt orange tartan ham";
-      const expected_author = "By: Zackary Jaskolski";
-    
-      expect(description).toEqual(expected_description);
-      expect(author).toEqual(expected_author);
-    });
-
-    it('second card has correct content', () => {
-      debugger
-      const props_1 = { deck: mock_games_index_request.arr_of_postgame_card_set[0], 
-        current_user_info: mock_games_index_request.current_user_info }
-      const slideshow_component_1 = mount(<Slideshow {...props_1} />);
-      
-      const expected_url = "/rails/active_storage/blobs/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBOZz09IiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--0f3f1adfa5d3d579512daee958eacd7b6a98d277/provider_avatar.jpg?disposition=attachment"
-      const expected_author = "By: Devon Kreiger I";
-
-      const carousel_item = slideshow_component_1.find('.carousel .carousel-item').at(1);
-      const url = carousel_item.find('img').prop('src');
-      const author = carousel_item.find('p').at(1).text();
-
-      expect(url).toEqual(expected_url);
-      expect(author).toEqual(expected_author);
+        expect(description).toEqual(expected_description);
+        expect(author).toEqual(expected_author);
+      });
     })
-
-    it('third card has correct content', () => {});
   })
 });
