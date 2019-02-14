178b4b73e6dadc4a0832aacc190ba1e3d7d22e42
Joe Noonan <joenoonan27@gmail.com>
2019-02-14 16:54:44 -0600

    fix SlideshowList.spec.js after mock_games_index_request updated

--- spec/javascript/packs/SlideshowList.spec.js
+++ spec/javascript/packs/SlideshowList.spec.js
@@ -6,11 +6,15 @@ import {mock_games_index_request} from '../mock_games_index_request';
 
 
 describe('SlideshowList component', () => {
-  it("3 Slideshow components created with correct params", () => {
-    const props = { arr_of_decks_of_cards: mock_games_index_request.arr_of_postgame_card_set, 
-      current_user_info: mock_games_index_request.current_user_info, 
-      needsAKeyBasedOnCardIds: true
-     };
+  it("3 Slideshow components will be created with correct params", () => {
+     const current_postgame_id = mock_games_index_request.PostGameTab.current_postgame_id
+     const decks_from_current_postgame = mock_games_index_request.PostGameTab.storage_of_viewed_postgames[current_postgame_id];
+
+     const props = { arr_of_decks_of_cards: decks_from_current_postgame, 
+                     current_user_info: mock_games_index_request.current_user_info }
+
+
+
     const slideshow_component = shallow(<SlideshowList {...props} />);
     expect(slideshow_component.find('Slideshow').length).toEqual(3);
 
