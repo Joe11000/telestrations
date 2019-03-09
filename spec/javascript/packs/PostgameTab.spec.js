ac653d91334721d35048f7e376d054c50de83546
Joe Noonan <joenoonan27@gmail.com>
2019-03-08 22:08:54 -0600

    update

--- spec/javascript/packs/PostgameTab.spec.js
+++ spec/javascript/packs/PostgameTab.spec.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {shallow} from 'enzyme';
 import PostGameTab from 'packs/postgame/TabBody/PostGameTab';
-import { mock_games_show_request } from '../mock_games_show_request';
+import { mock_games_show_request_for_last_postgame } from '../mock_games_show_request_for_last_postgame';
 
 // import enzymeSerializer from 'enzyme-to-json/serializer';
 // expect.addSnapshotSerializer(enzymeSerializer)
@@ -10,10 +10,10 @@ describe('PostGameTab Component', () => {
 
   it('renders card title', () => {
     const mockRetrieveCardsForPostgame = jest.fn();
-    const post_game_tab_props = { 
-                                  ...mock_games_show_request.PostGameTab, 
+    const post_game_tab_props = {
+                                  ...mock_games_show_request_for_last_postgame.PostGameTab,
                                   retrieveCardsForPostgame: mockRetrieveCardsForPostgame
-                                }                              
+                                }
     const post_game_tab_component = shallow(<PostGameTab {...post_game_tab_props } />);
 
     expect(post_game_tab_component.find('CardTitle').children().text()).toEqual('Post Game Results');
@@ -21,25 +21,25 @@ describe('PostGameTab Component', () => {
 
   it('renders helpful information on screen', () => {
     const mockRetrieveCardsForPostgame = jest.fn();
-    const post_game_tab_props = { 
-                                  ...mock_games_show_request.PostGameTab, 
+    const post_game_tab_props = {
+                                  ...mock_games_show_request_for_last_postgame.PostGameTab,
                                   retrieveCardsForPostgame: mockRetrieveCardsForPostgame
-                                }                              
+                                }
     const post_game_tab_component = shallow(<PostGameTab {...post_game_tab_props } />);
     expect(post_game_tab_component.find('p').text()).toEqual('( * cards with red text were made by you * )');
   })
 
   it('renders the GameSelector with correct props and a key', () => {
     const mockRetrieveCardsForPostgame = jest.fn();
-    const post_game_tab_props = { 
-                                  ...mock_games_show_request.PostGameTab, 
+    const post_game_tab_props = {
+                                  ...mock_games_show_request_for_last_postgame.PostGameTab,
                                   retrieveCardsForPostgame: mockRetrieveCardsForPostgame
-                                }                              
+                                }
     const post_game_tab_component = shallow(<PostGameTab {...post_game_tab_props } />);
 
     const expected_game_selector_props = {
-      all_postgames_of__current_user: mock_games_show_request.OutOfGameCardUploadTab.out_of_game_cards,
-      current_postgame_id: mock_games_show_request.PostGameTab.current_postgame_id,
+      all_postgames_of__current_user: mock_games_show_request_for_last_postgame.OutOfGameCardUploadTab.out_of_game_cards,
+      current_postgame_id: mock_games_show_request_for_last_postgame.PostGameTab.current_postgame_id,
       retrieveCardsForPostgame: mockRetrieveCardsForPostgame,
     }
 
@@ -50,21 +50,21 @@ describe('PostGameTab Component', () => {
 
   it('creates the list of slideshows that receive the correct props ', () => {
     const mockRetrieveCardsForPostgame = jest.fn();
-    
-    const post_game_tab_props = { 
-                                  ...mock_games_show_request.PostGameTab, 
-                                  retrieveCardsForPostgame: mockRetrieveCardsForPostgame, 
-                                  current_user_info: { ...mock_games_show_request.current_user_info }
+
+    const post_game_tab_props = {
+                                  ...mock_games_show_request_for_last_postgame.PostGameTab,
+                                  retrieveCardsForPostgame: mockRetrieveCardsForPostgame,
+                                  current_user_info: { ...mock_games_show_request_for_last_postgame.current_user_info }
                                 };
-                                
+
     const post_game_tab_component = shallow(<PostGameTab {...post_game_tab_props } />);
-    let current_postgame_id = mock_games_show_request.PostGameTab.current_postgame_id;
-    let arr_of_decks_of_cards = mock_games_show_request.PostGameTab.storage_of_viewed_postgames[current_postgame_id];
+    let current_postgame_id = mock_games_show_request_for_last_postgame.PostGameTab.current_postgame_id;
+    let arr_of_decks_of_cards = mock_games_show_request_for_last_postgame.PostGameTab.storage_of_viewed_postgames[current_postgame_id];
     const expected_slideshow_list_props = {
-                                            current_user_info: { ...mock_games_show_request.current_user_info }, 
+                                            current_user_info: { ...mock_games_show_request_for_last_postgame.current_user_info },
                                             arr_of_decks_of_cards
                                           };
-    
+
     const SlideshowList = post_game_tab_component.find('SlideshowList');
     expect(SlideshowList.length).toEqual(1);
     expect(SlideshowList.key()).toEqual('slideshow_list_7');
