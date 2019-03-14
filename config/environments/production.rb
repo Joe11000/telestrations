baac9e513d5b03cfb61bcd2707a1d0e35de18834
Joe Noonan <joenoonan27@gmail.com>
2019-03-14 18:05:50 -0500

    Force SSL setting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -169,7 +169,7 @@ Rails.application.configure do
   config.action_cable.allowed_request_origins = [ /https:\/\/telestrations.herokuapp.com*/, /http:\/\/telestrations.herokuapp.com*/ ]
 
 #   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-#   config.force_ssl = true
+  config.force_ssl = true
 
 #   # Use the lowest log level to ensure availability of diagnostic information
 #   # when problems arise.
